21c78f923b51ca390a709345d8d0099298dd2e44
macielexpedito <macielexpedito@gmail.com>
2026-04-10 22:18:14 -0300

    loop

--- for_while/script.js
+++ for_while/script.js
@@ -1,40 +1,61 @@
-// for (let contador = 10; contador >= 0; contador--) {
-//     console.log("Item " + contador);
-// }
+// // for (let contador = 10; contador >= 0; contador--) {
+// //     console.log("Item " + contador);
+// // }
 
-const frutas = ["banana", "melão", "abacaxi", "pera","uva"];
+// const frutas = ["banana", "melão", "abacaxi", "pera","uva"];
 
-//console.log(frutas.length);
+// //console.log(frutas.length);
 
- const cache = frutas.length;
+//  const cache = frutas.length;
 
-// for (let contador = 0; contador <= cache; contador++) {
-//     console.log(frutas[contador]);
+// // for (let contador = 0; contador <= cache; contador++) {
+// //     console.log(frutas[contador]);
     
+// // }
+
+// // for (const fruta of frutas) {
+// //     console.log(fruta);
+// // }
+
+// // for (let contador = 0;contador< cache; contador++) {
+// //     if (contador % 2 == 0) {
+// //          console.log(frutas[contador]);
+// //     }
+// // }
+
+// // const cliente = {
+// //     nome: "João",
+// //     idade: 30,
+// //     cidade: "osasco",
+// //     };
+// //     for (const dados in cliente) {
+// //         console.log(dados);
+// //         console.log(`${dados}: ${cliente[dados]}`);
+// //     }
+
+// let contador = 10;
+// while (contador >= 0) {
+//     console.log( contador);
+//     contador--;
 // }
 
-// for (const fruta of frutas) {
-//     console.log(fruta);
+// console.log("OK");
+// let entradaUsuario = +prompt("Digite um número:");
+// console.log(entradaUsuario);
+
+// let valorDigitado;
+// while (entradaUsuario != 2 && entradaUsuario != 4 && entradaUsuario != 6) {
+// console.log((entradaUsuario = +prompt("Digite um número:")));
 // }
 
-// for (let contador = 0;contador< cache; contador++) {
-//     if (contador % 2 == 0) {
-//          console.log(frutas[contador]);
-//     }
-// }
+let saldo = 10000;
 
-// const cliente = {
-//     nome: "João",
-//     idade: 30,
-//     cidade: "osasco",
-//     };
-//     for (const dados in cliente) {
-//         console.log(dados);
-//         console.log(`${dados}: ${cliente[dados]}`);
-//     }
+let saque = +prompt("Digite o valor do saque: ");
 
-let contador = 10;
-while (contador >= 0) {
-    console.log( contador);
-    contador--;
+
+while (saldo >= saque) {
+    saldo -= saque;
+    console.log("saque de ${saque} realizado com sucesso");
+    console.log("saldo atual ${saldo}");
 }
+
