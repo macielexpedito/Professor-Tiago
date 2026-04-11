fec81faeb590549ec40535f2eec7d1b3895cc7a5
macielexpedito <macielexpedito@gmail.com>
2026-04-10 21:44:00 -0300

    comit inicial

--- for_while/script.js
+++ for_while/script.js
@@ -1,1 +1,40 @@
-console.log("")
+// for (let contador = 10; contador >= 0; contador--) {
+//     console.log("Item " + contador);
+// }
+
+const frutas = ["banana", "melão", "abacaxi", "pera","uva"];
+
+//console.log(frutas.length);
+
+ const cache = frutas.length;
+
+// for (let contador = 0; contador <= cache; contador++) {
+//     console.log(frutas[contador]);
+    
+// }
+
+// for (const fruta of frutas) {
+//     console.log(fruta);
+// }
+
+// for (let contador = 0;contador< cache; contador++) {
+//     if (contador % 2 == 0) {
+//          console.log(frutas[contador]);
+//     }
+// }
+
+// const cliente = {
+//     nome: "João",
+//     idade: 30,
+//     cidade: "osasco",
+//     };
+//     for (const dados in cliente) {
+//         console.log(dados);
+//         console.log(`${dados}: ${cliente[dados]}`);
+//     }
+
+let contador = 10;
+while (contador >= 0) {
+    console.log( contador);
+    contador--;
+}
