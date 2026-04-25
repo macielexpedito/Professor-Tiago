3ec4d53cdf4daa0891b3470f97229885b76cda0b
macielexpedito <macielexpedito@gmail.com>
2026-04-24 21:56:46 -0300

    string

---
+++ TRABALHANDO STRINGS/string.js
@@ -0,0 +1,40 @@
+let nome = "Pedro";
+
+console.log(nome[2]);
+console.log(nome.length);
+
+let maiusculo = nome.toUpperCase();
+console.log(maiusculo);
+console.log(nome);
+
+/*
+let emailDigitado = "   Caique@GooGle.coM    ";
+console.log(emailDigitado.length);
+
+let emailCadastrado = "caique@google.com";
+console.log(emailCadastrado.length);
+
+let emailSemEspaco = emailDigitado.trim();
+console.log(emailSemEspaco);
+
+let emailNormalizado = emailSemEspaco.toLowerCase();
+console.log(emailNormalizado);
+
+let emailNormalizado = emailDigitado.trim().toLowerCase();
+console.log(emailNormalizado);
+
+let pedido = "7";
+let pedidoNormalizado = pedido.padStart(5, "X").padEnd(8, "Y");
+console.log(pedidoNormalizado);
+
+
+let nomeArquivo = "relatorio_2026.xls";
+
+let arquivoRecebido = nomeArquivo.includes(".xls");
+console.log(arquivoRecebido);
+*/
+
+let frase = "O Linux manda!";
+let palavras = frase.split(" ");
+console.log(palavras);
+
