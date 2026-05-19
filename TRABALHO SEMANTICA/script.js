31f8e909a3456d32960ca30206dd06fbd3f8a9d9
macielexpedito <macielexpedito@gmail.com>
2026-05-19 18:57:15 -0300

    armazenar api

--- TRABALHO SEMANTICA/script.js
+++ TRABALHO SEMANTICA/script.js
@@ -12,6 +12,18 @@ const origem = document.querySelector('.origem');
 const voltar = document.querySelectorAll('main button')[0];
 const avancar = document.querySelectorAll('main button')[1];
 
-let personagens = [];
-let indice = 0;
+let personagens = []; /* Array para armazenar os personagens retornados pela API */
+let indice = 0; /* Índice para controlar a navegação entre os personagens */
+
+buscar.addEventListener('click', buscarPersonagem); /* Evento de clique para buscar o personagem */
+
+async function buscarPersonagem() {
+    const valor = input.value; /* Obtém o valor do input */
+
+        try { /* Tenta buscar os personagens na API */
+
+            let resposta; /* Variável para armazenar a resposta da API */
+            
+    
+    
 
